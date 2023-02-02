3ea9d78400bcad650b17d42c68dbdaf72ce44c71
boura0013 <bourdeaux.adrien@gmail.com>
2023-02-02 16:54:58 -0500

    Updated exception and out-of-bounds checks

--- popup.js
+++ popup.js
@@ -1,14 +1,23 @@
 // Add onclick listeners to buttons
 document.getElementById("startTimerButton").onclick = function(){
     console.log("Timer start button clicked");
-    var HoursInputFieldValue = parseInt(document.getElementById("timerInputHoursField").value);
-    var MinutesInputFieldValue = parseInt(document.getElementById("timerInputMinutesField").value);
-    var SecondsInputFieldValue = parseInt(document.getElementById("timerInputSecondsField").value);
-    var HoursInputFieldValueString = document.getElementById("timerInputHoursField").value;
-    var MinutesInputFieldValueString = document.getElementById("timerInputMinutesField").value;
-    var SecondsInputFieldValueString = document.getElementById("timerInputSecondsField").value;
+    var exceptionCheckPassed = true;
+    var HoursInputFieldValue = (parseInt(document.getElementById("timerInputHoursField").value)).toString();
+    var MinutesInputFieldValue = (parseInt(document.getElementById("timerInputMinutesField").value)).toString();
+    var SecondsInputFieldValue = (parseInt(document.getElementById("timerInputSecondsField").value)).toString();
     console.log("Hours = " + HoursInputFieldValue + " Minutes = " + MinutesInputFieldValue + " Seconds = " + SecondsInputFieldValue);
-    if(!(HoursInputFieldValue < 0 || HoursInputFieldValueString.indexOf("NaN") == -1 || MinutesInputFieldValue < 0 || MinutesInputFieldValueString.indexOf("NaN") == -1 || SecondsInputFieldValue < 0 || SecondsInputFieldValueString.indexOf("NaN") == -1)){
+    // This if statement checks for if there are any numeric characters or if the number is less than 0. If true, then fails and gives a console message
+    if(HoursInputFieldValue < 0 || HoursInputFieldValue.indexOf("NaN") > -1 || MinutesInputFieldValue < 0 || MinutesInputFieldValue.indexOf("NaN") > -1 || SecondsInputFieldValue < 0 || SecondsInputFieldValue.indexOf("NaN") > -1){
+        console.log("Exception check failed");
+        exceptionCheckPassed = false;
+    }
+    else if(exceptionCheckPassed){
+        // The variable will be true if the above block did not trigger
+        // If input is valid, code here executes
         console.log("Exception check passed");
+        var totalSeconds = ((parseInt(HoursInputFieldValue) * 60 * 60) + (parseInt(MinutesInputFieldValue) * 60) + parseInt(SecondsInputFieldValue));
+        console.log("Total seconds: " + totalSeconds);
+        console.log("Hours: " + HoursInputFieldValue + " Minutes: " + MinutesInputFieldValue + " Seconds: " + SecondsInputFieldValue);
+        console.log(MinutesInputFieldValue.indexOf("NaN"));
     }
 }
