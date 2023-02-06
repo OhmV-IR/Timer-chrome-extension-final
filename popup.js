945499deefa2d2a19fd57c4b5aa2df1ca11844d7
boura0013 <bourdeaux.adrien@gmail.com>
2023-02-06 11:50:35 -0500

    More comments

--- popup.js
+++ popup.js
@@ -34,14 +34,17 @@ function formatTime(h,m,s){
     return output;
 }
 function lowerTime(){
+    // If curentSeconds has not yet reached 0(there are seconds left), then take one off of the remaining seconds 
     if(currentSeconds != 0){
         currentSeconds = currentSeconds - 1;
     }
+    // If there are no seconds left but there are minutes left then take off 1 minute and add 60 seconds then subtract 1 from that
     else if(currentMinutes != 0){
         currentMinutes = currentMinutes - 1;
         currentSeconds = 60;
         currentSeconds  = currentSeconds - 1;
     }
+    // If there are no minutes left but there are hours left then take off 1 hour, add 60 minutes remove 1 minute then add 60 seconds and remove 1 second
     else if(currentHours != 0){
         currentHours = currentHours - 1;
         currentMinutes = 60;
@@ -49,24 +52,34 @@ function lowerTime(){
         currentSeconds = 60;
         currentSeconds = currentSeconds - 1;
     }
+    // If none of these are true, then there are 0 seconds, 0 minutes and 0 hours left in the timer and it is finished
     else{
+        // Send console message
         console.log("Timer complete");
-        document.getElementById("timerDisplay").innerHTML = "0:00:00"
+        // Set timer display to default
+        document.getElementById("timerDisplay").innerHTML = "0:00:00";
+        // Stop this code from continuing to execute
         clearInterval(intervalID);
+        // Set input fields to 0
         document.getElementById("timerInputHoursField").value = "0";
         document.getElementById("timerInputMinutesField").value = "0";
         document.getElementById("timerInputSecondsField").value = "0";
     }
 }
+// When start timer button is pressed run code in brackets
 document.getElementById("startTimerButton").onclick = function(){
     console.log("Timer start button clicked");
+    // Initialize variables
+    // parseInt() converts string to integer and returns NaN(a string) if not a valid number and even if it is a number, we use the toString() function to convert the number back to a string
+    // This essentially will change any non-number input to NaN which we search for in our exception checks later
     var exceptionCheckPassed = true;
     var HoursInputFieldValue = (parseInt(document.getElementById("timerInputHoursField").value)).toString();
     var MinutesInputFieldValue = (parseInt(document.getElementById("timerInputMinutesField").value)).toString();
     var SecondsInputFieldValue = (parseInt(document.getElementById("timerInputSecondsField").value)).toString();
+    // Get timer display into variable
     var timerDisplay = document.getElementById("timerDisplay");
     console.log("Hours = " + HoursInputFieldValue + " Minutes = " + MinutesInputFieldValue + " Seconds = " + SecondsInputFieldValue);
-    // This if statement checks for if there are any numeric characters or if the number is less than 0. If true, then fails and gives a console message
+    // This if statement checks for the output of NaN by parseInt() for invalid integer
     if(HoursInputFieldValue < 0 || HoursInputFieldValue.indexOf("NaN") > -1 || MinutesInputFieldValue < 0 || MinutesInputFieldValue.indexOf("NaN") > -1 || SecondsInputFieldValue < 0 || SecondsInputFieldValue.indexOf("NaN") > -1){
         console.log("Exception check failed");
         exceptionCheckPassed = false;
